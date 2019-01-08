f953d661ab2c449ea32153a5dcd0794959e2997d
Fabian Woitschek <fabianwt@cc-4.pc2.uni-paderborn.de>
2019-01-08 10:48:17 +0100

    Add code for sobel filter on cpu gor gimp

---
+++ module_3/sobel_cpu/sobel_cpu.c
@@ -0,0 +1,202 @@
+#include <libgimp/gimp.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+
+static void init(void);
+static void quit(void);
+static void query(void);
+static void run(const gchar *name, gint nparams, const GimpParam *param, gint *nreturn_vals, GimpParam **return_vals);
+
+static void time_measure(struct timespec *time);
+static gdouble time_diff(struct timespec *start, struct timespec *end);
+
+static void sobel_cpu (GimpDrawable *drawable);
+
+GimpPlugInInfo PLUG_IN_INFO = {
+	init,
+	quit,
+	query,
+	run
+};
+
+MAIN()
+
+static void init(void) {}
+static void quit(void) {}
+
+static void query(void) {
+	static GimpParamDef args[] = {
+		{
+			GIMP_PDB_INT32,
+			"run-mode",
+			"Run mode"
+		},{
+			GIMP_PDB_IMAGE,
+			"image",
+			"Input image"
+		},{
+			GIMP_PDB_DRAWABLE,
+			"drawable",
+			"Input drawable"
+		}
+	};
+	
+	gimp_install_procedure (
+		"sobel_cpu",
+		"Sobel Edge Detection Operator on CPU.",
+		"Performs Sobel Operator on an Input Image.",
+		"Achim Loesch",
+		"Copyright Achim Loesch",
+		"2017",
+		"Sobel@CPU...",
+		"RGBA",
+		GIMP_PLUGIN,
+		G_N_ELEMENTS(args), 0,
+		args, NULL
+	);
+	
+	gimp_plugin_menu_register (
+		"sobel_cpu",
+		"<Image>/Filters/Misc"
+	);
+}
+
+static void run(const gchar *name, gint nparams, const GimpParam *param, gint *nreturn_vals, GimpParam **return_vals) {
+	static GimpParam  values[1];
+	GimpPDBStatusType status = GIMP_PDB_SUCCESS;
+	GimpRunMode       run_mode;
+	GimpDrawable *drawable;
+	
+	*nreturn_vals	= 1;
+	*return_vals	= values;
+
+	values[0].type	= GIMP_PDB_STATUS;
+	values[0].data.d_status = status;
+	
+	run_mode	= param[0].data.d_int32;
+	
+	drawable	= gimp_drawable_get(param[2].data.d_drawable);
+	
+	sobel_cpu(drawable);
+	
+	gimp_displays_flush();
+	gimp_drawable_detach(drawable);
+}
+
+static void time_measure(struct timespec *time) {
+	clock_gettime(CLOCK_REALTIME, time);
+}
+
+static gdouble time_diff(struct timespec *start, struct timespec *end) {
+	struct timespec temp;
+	gdouble diff = 0.0;
+	
+	if ((end->tv_nsec - start->tv_nsec)<0) {
+		temp.tv_sec  =              end->tv_sec  - start->tv_sec  - 1;
+		temp.tv_nsec = 1000000000 + end->tv_nsec - start->tv_nsec;
+	} else {
+		temp.tv_sec  = end->tv_sec  - start->tv_sec;
+		temp.tv_nsec = end->tv_nsec - start->tv_nsec;
+	}
+	
+	diff += (gdouble)temp.tv_sec;
+	diff += (gdouble)temp.tv_nsec / 1000000000.0;
+	
+	return diff;
+}
+
+static void sobel_cpu(GimpDrawable *drawable) {
+	struct timespec start;
+	struct timespec end;
+	gdouble diff	= 0.0;
+	
+	gint i, j, channels;
+	gint x1, y1, x2, y2;
+	GimpPixelRgn rgn_in, rgn_out;
+	
+	guchar *image_in	= NULL;
+	guchar *image_out	= NULL;
+	
+	gimp_drawable_mask_bounds(drawable->drawable_id, &x1, &y1, &x2, &y2);
+	
+	gimp_pixel_rgn_init(&rgn_in,  drawable, x1, y1, x2-x1, y2-y1, FALSE, FALSE);
+	gimp_pixel_rgn_init(&rgn_out, drawable, x1, y1, x2-x1, y2-y1,  TRUE,  TRUE);
+	
+	channels	= gimp_drawable_bpp(drawable->drawable_id);
+	
+	image_in	= g_new(guchar, x2 * y2 * channels);
+	image_out	= g_new(guchar, x2 * y2 * channels);
+	
+	gimp_pixel_rgn_get_rect(&rgn_in, image_in, x1, y1, x2-x1, y2-y1);
+	
+	/*
+	 * In order to iterate images use i and j to access pixels.
+	 *
+	 *  x1 <= j < x2
+	 * /------------/ y1
+	 * /            / <=
+	 * /            / i
+	 * /            / <
+	 * /------------/ y2
+	 *
+	 */
+	
+	time_measure(&start);
+	
+	gint filter_x[] = { 1,  2,  1,  0,  0,  0, -1, -2, -1};
+	gint filter_y[] = { 1,  0, -1,  2,  0, -2,  1,  0, -1};
+	
+	for (i=y1; i<y2; i++) {
+		if (i % ((y2-y1)/10) == 0) {
+			gimp_progress_update((gdouble)i / (gdouble)(y2 - y1));
+		}
+		for (j=x1; j<x2; j++) {
+			/*
+			 * ADD CODE HERE.
+			 */
+			// boundary check and zero
+			if (i == y1 || i == (y2-1) || j == x1 || j == (x2-1)){
+				image_out[(j+x2*i)*4+0] = 0;
+				image_out[(j+x2*i)*4+1] = 0;
+				image_out[(j+x2*i)*4+2] = 0;	
+				image_out[(j+x2*i)*4+3] = 255;
+			}
+			else{
+		                gint c, k, l;
+				for (c=0; c<4; c++){
+					if (c != 3){
+						gint result_x = 0;
+						gint result_y = 0;
+						for (k=0; k<3; k++){
+							for (l=0; l<3; l++){
+								result_x += filter_x[k*3+l] * image_in[((j+x2*i-1-x2)+l+k*x2)*4+c];
+								result_y += filter_y[k*3+l] * image_in[((j+x2*i-1-x2)+l+k*x2)*4+c];
+							}			
+						}
+						image_out[(j+x2*i)*4+c] =  abs(result_x) + abs(result_y);
+						//image_out[(j+x2*i)*4+c] = sqrt(pow(result_x, 2) + pow(result_y, 2));
+					} else{
+					image_out[(j+x2*i)*4+c] =  255;
+					}
+				}
+			}
+		}
+	}
+	
+	time_measure(&end);
+	diff = time_diff(&start, &end);
+	
+	gimp_pixel_rgn_set_rect(&rgn_out, image_out, x1, y1, x2-x1, y2-y1);
+	
+	gimp_drawable_flush(drawable);
+	gimp_drawable_merge_shadow(drawable->drawable_id, TRUE);
+	gimp_drawable_update(drawable->drawable_id, x1, y1, x2-x1, y2-y1);
+	
+	g_free(image_in);
+	g_free(image_out);
+	
+	g_message("Execution time: %lf s.\n", diff);
+}
